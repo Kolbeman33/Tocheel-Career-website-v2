f0d4fc1220cb03635ea5e9caad3d67c6f35ac8d1
Maxikolbe <125804860+Kolbeman33@users.noreply.github.com>
2023-02-24 01:15:36 +0000

    added to html and css

--- app.py
+++ app.py
@@ -2,9 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = {
+  {
+  'id': 1,
+  'title': 'Data Scientist',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 10,00,000'
+},
+{
+  'id': 2,
+  'title': 'Data Analyst',
+  'location': 'Delhi, India',
+  'salary': 'Rs. 15,00,000'
+},
+{
+  'id': 3,
+  'title': 'frontend Engineer',
+  'location': 'Remote',
+  'salary': 'Rs. 12,00,000'
+},
+{
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': 'San Francisco, USA',
+  'salary': 'Rs. $2,00,000'
+}
+},
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', 
+                         jobs=JOBS, company_name='Tocheel')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
