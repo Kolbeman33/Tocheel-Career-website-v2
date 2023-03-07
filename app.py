0b1b61f9119b13a0d0206d5f5803e07b1401b004
Maxikolbe <125804860+Kolbeman33@users.noreply.github.com>
2023-03-07 17:19:38 +0000

    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'Location': 'Lagos, Nigeria',
-    'Salary': 'Rs. 500,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'Location': 'Abuja, Nigeria',
-    'Salary': 'Rs. 550,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'Location': 'Remote',
-    'Salary': 'Rs. $120,000'
-  },
-  {
-    'id': 4,
-    'title': 'Backtend Engineer',
-    'Location': 'Enugu, Nigeria',
-    'Salary': 'Rs. $50,000'
-  }
+{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Lagos, Nigeria',
+  'salary': '#550,000'
+},
+{
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Abuja, Nigeria',
+  'salary': '#610,000'
+},
+{
+  'id': 3,
+  'title': 'Frontend Engineer',
+  'location': 'Remote',
+},
+{
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': 'Enugu, Nigeria',
+  'salary': '#570,000'
+},
 ]
 
 @app.route("/")
 def hello_Tocheel():
-  return render_template('home.html', jobs=JOBS,
-                          company_name='Tocheel')
+  return render_template('home.html',
+                        jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
