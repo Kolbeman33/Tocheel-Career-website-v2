c549d52ae27a46b7da4d794d8b676c5554a8c1b9
Maxikolbe <125804860+Kolbeman33@users.noreply.github.com>
2023-03-07 14:19:31 +0000

    added to html and css

--- app.py
+++ app.py
@@ -2,37 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-JOBS = {
+JOBS = [
   {
-  'id': 1,
-  'title': 'Data Scientist',
-  'location': 'Bengaluru, India',
-  'salary': 'Rs. 10,00,000'
-},
-{
-  'id': 2,
-  'title': 'Data Analyst',
-  'location': 'Delhi, India',
-  'salary': 'Rs. 15,00,000'
-},
-{
-  'id': 3,
-  'title': 'frontend Engineer',
-  'location': 'Remote',
-  'salary': 'Rs. 12,00,000'
-},
-{
-  'id': 4,
-  'title': 'Backend Engineer',
-  'location': 'San Francisco, USA',
-  'salary': 'Rs. $2,00,000'
-}
-},
+    'id': 1,
+    'title': 'Data Analyst',
+    'Location': 'Lagos, Nigeria',
+    'Salary': 'Rs. 500,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'Location': 'Abuja, Nigeria',
+    'Salary': 'Rs. 550,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'Location': 'Remote',
+    'Salary': 'Rs. $120,000'
+  },
+  {
+    'id': 4,
+    'title': 'Backtend Engineer',
+    'Location': 'Enugu, Nigeria',
+    'Salary': 'Rs. $50,000'
+  }
+]
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html', 
-                         jobs=JOBS, company_name='Tocheel')
+def hello_Tocheel():
+  return render_template('home.html', jobs=JOBS,
+                          company_name='Tocheel')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
